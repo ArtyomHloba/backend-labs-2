292ace5c5f4f5a0c8451753884734aabeb429b46
ArtyomHloba <artemgloba2@gmail.com>
2025-03-03 12:46:55 +0200

    Add paginate users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { User } from '../entities/user.entity'
 import { CreateUserDto } from './create-user.dto'
+import { paginate, PaginateQuery } from 'nestjs-paginate'
 
 @Injectable()
 export class UsersService {
@@ -11,8 +12,12 @@ export class UsersService {
     private userRepository: Repository<User>,
   ) {}
 
-  async findAll (): Promise<User[]> {
-    return this.userRepository.find()
+  async findAll (query: PaginateQuery) {
+    return paginate(query, this.userRepository, {
+      sortableColumns: ['id', 'email'],
+      defaultSortBy: [['id', 'ASC']],
+      searchableColumns: ['email'],
+    })
   }
 
   async create (createUserDto: CreateUserDto): Promise<User> {
