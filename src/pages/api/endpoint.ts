6326f54fde9a9996441eafd79bab80b7ca32dc80
ArtyomHloba <artemgloba2@gmail.com>
2025-03-03 13:34:14 +0200

    add .env + endpoints

---
+++ src/pages/api/endpoint.ts
@@ -0,0 +1,21 @@
+import Cors from 'cors'
+
+const cors = Cors({
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+})
+
+function runMiddleware (req, res, fn) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, result => {
+      if (result instanceof Error) {
+        return reject(result)
+      }
+      return resolve(result)
+    })
+  })
+}
+
+export default async function handler (req, res) {
+  await runMiddleware(req, res, cors)
+  res.json({ message: 'API works!' })
+}
